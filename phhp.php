0d40d672de0de42cea865d9e9341e660c8bc8560
Catherine McMahon <disbemygehmail@gmail.com>
2015-11-09 22:02:28 -0800

    fixing php errors

--- phhp.php
+++ phhp.php
@@ -1,3 +1,81 @@
+	<?php
+		$output = '';
+		$style = '';
+
+			 if(isset($_GET['input'])) {
+			 	$userInput = $_GET['input'];
+
+			if($selectOption = $_GET['selectOption']=='bgcolor') {
+				$style = "style = 'background-color:$userInput;'";
+			} else if ($_GET['selectOption']=='palindrome') {
+					$array = array();
+					for($i=0; $i<strlen($userInput); $i++) {
+						for ($j = 0; $j < strlen($userInput); $j++) {
+							$word = $substr($userInput, $i, $j);
+							if (strlen($word) >= 2) {
+								$reverse = strrev($word); 
+								if($word == $reverse) {
+									array_push($array, $word);
+								}
+							}
+						}
+					}
+
+					$output = implode(',', $array);
+			 } else if($_GET['selectOption']=='list') {
+			 	// $strlen = strlen($userInput);
+			 	// for( $i = 0; $i <= $strlen; $i++ ) {
+	 			// 	$char = substr( $userInput, $i, 1);
+	 			// }
+	 			// for($i=0; $i<$strlen; $i++) {
+	 			// 	if($char!=',') {
+	 			// 	$count++;
+	 			// } else {
+	 			// }
+
+					// removes commas, saves strings to new array		 	
+					$array = str_split ($userInput);
+					foreach($array as $a) {
+						while($a !=',') {
+							$string += str_pad($a, 1);
+						}
+						if($a ==',') {
+							$array_push($newArray, $string);
+							$array = str_replace($array, ',', '');
+						}
+					}
+					// removes unique values, returns array with unduplicated strings
+					$output = implode(',', $array);
+
+					// $array = explode(",", $userInput);
+					// $newArray = array_unique($newArray);
+					// echo $newArray;				
+			} elseif($_GET['selectOption']=='integer') { 
+					if(is_numeric($userInput)==false) {
+						echo('Error: Input an integer. (6, 10, 200...)');
+					} else {
+						$userInput = intval($userInput);
+						$factorSum = 0;
+						for($i=0; $i<$userInput; $i++) {
+							$factor = $userInput % $i;
+							if($factor === 0) {
+								$factorSum += $i;
+							}
+						}
+
+						if($userInput === $factorSum) {
+							$output = "$userInput is a perfect number.";
+						} else {
+							$output = "$userInput is not a perfect number.";
+						}
+					}				
+				}
+
+			}
+		?>
+
+
+
 <html>
 <head>
 
@@ -6,8 +84,7 @@
 
 </head>
 
-<body>
-
+<body>	
 	<div class="header">
 		<h1 >Catherine McMahon</h1>
 	</div>
@@ -20,87 +97,34 @@
 	</ul>
 	</div>
 
-	<div class="content" <?php print($style)?>>
+	<div class="content" <?php echo $style ?>>
 		<h1>PHP</h1>
+		<a href="html.html">HTML</a>
+		<a href="phhp.php">PHP</a>
+		<a href="js.html">JS</a>
 
-		<p>
-			Algorithms to Implement:
-			<select name="selectOption">
-			  <option value="integer">Integer</option>
-			  <option value="palindrome">Palindrome</option>
-			  <option value="list">List</option>
-			  <option value="bgcolor">BGColor</option>
-			</select>
-		</p>
+		
 
 		<form action="phhp.php" method="GET">
-			Info Collector?: <input type="text" name="input"><br>
+			<p>
+				Algorithms to Implement:
+				<select name="selectOption">
+				  <option value="integer">Integer</option>
+				  <option value="palindrome">Palindrome</option>
+				  <option value="list">List</option>
+				  <option value="bgcolor">BGColor</option>
+				</select>
+			</p>
+			Information Collector: <input type="text" name="input"><br>
 			<input type="submit">
 		</form>
 
+		<p>
+	
 		<?php
-			if(isset($_GET['input'])) {
-			$userInput = $_GET['input'];
-		   	print('This is user input: ' + "$userInput");
-
-		   	if($selectOption = $_GET['taskOption']=='bgcolor'){
-			function changeBGColor( $userInput ) { 
-				$style = '';
-				if(strlen($userInput)!=6) {
-					print('Error: input a string of hexadecimal digits. (6 digits)');
-				} else {
-					$style = "style = 'background-color: $userInput';";
-				}
-			}
-
-		} elseif($selectOption = $_GET['taskOption']=='palindrome') {
-			function Palindrome( $userInput ) { 
-				$array = array();
-				$reverse = strrev($userInput);
-				$arrIndex = 0;
-				$word = string;
-				for($i=0; $i<(strlen($userInput)); $i++) {
-					$word = $userInput(string $string, int $i, int($i+2));
-					$reverse = strrev($word);
-					if($word == $reverse) {
-						$array[] = $word;
-					}
-				}
-				return $array;
-			}
-
-		} elseif($selectOption = $_GET['taskOption']=='list'){
-			function List( $userInput ) { 
-				$array = str_split ( $userInput );
-				for($i=0; $i<count($array); $i++) {
-					// if()
-				}
-			}
-
-		} elseif($selectOption = $_GET['taskOption']=='integer'){ 
-			function Integer( $userInput ) { 
-				if(is_int($userInput)==true) {
-					print('Error: Input an integer. (6, 10, 200...)');
-				} else {
-					$sum = 0;
-					for($i=0; $i<$userInput; $i++) {
-						$factor = $userInput/$i;
-						if(is_int($factor) == true) {
-							$sum = $factor + $sum;
-						} else {
-							// do nothing
-						}
-				}
-
-				if($userInput > $sum) {
-					print("$userInput" + ' is a perfect number.');
-				} else {
-					print("$userInput" + ' is not a perfect number.');
-				}
-			}
-		}
-	}
-	?>
+			print("OUTPUT: $output");
+		?>
+		</p>
 	
 	</div>
 
